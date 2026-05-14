c3560f74bee1dcb1f6ed302e197f735383d6da13
Шапкин Николай <kosmys122@gmail.com>
2026-05-14 20:50:32 +0300

    Update SoundService to use audioplayers for Web and Android

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -40,7 +40,7 @@ class _SlotMachineState extends State<SlotMachine> {
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning) return;
-    SoundService.playClick();
+    await SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
@@ -62,25 +62,30 @@ class _SlotMachineState extends State<SlotMachine> {
       onTick: (val) => setState(() => _slot3 = val),
     );
     await Future.delayed(Duration(milliseconds: 300));
-    setState(() {
-      _isSpinning = false;
+    String newMessage;
+    int coinsChange;
       if (result1 == result2 && result2 == result3) {
         if (result1 == 'assets/images/seven.png') {
-          _coins += 10;
-          _message = 'JACKPOT!!! +10 монет';
-          SoundService.playJackpot();
+          coinsChange = 10;
+          newMessage = 'JACKPOT!!! 🎰🎰🎰 +10 монет';
+          await SoundService.playJackpot();
         } else {
-          _coins += 3;
-          _message = 'Победа! +3 монеты';
-          SoundService.playWin();
+          coinsChange = 3;
+          newMessage = 'Победа! 🎉 +3 монеты';
+          await SoundService.playWin();
         }
       } else {
-        _coins -= 1;
-        _message = 'Попробуй еще раз -1 монета';
-        SoundService.playLose();
+        coinsChange = -1;
+        newMessage = 'Попробуй еще раз 😩 -1 монета';
+        await SoundService.playLose();
       }
-    });
+      setState(() {
+        _isSpinning = false;
+        _coins += coinsChange;
+        _message = newMessage;
+      });
   }
+  
 
   void _reset() {
     setState(() {
